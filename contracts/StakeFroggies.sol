05a1043511b0a7edd55ef2c2be26cfa7cb10230e
Alfonso <alfonso@viruett.com>
2022-04-07 19:12:52 -0700

    adds skeleton

--- contracts/StakeFroggies.sol
+++ contracts/StakeFroggies.sol
@@ -27,3 +27,25 @@
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
+
+import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract StakeFroggies is IERC721Receiver, Ownable {
+
+    address public froggyAddress;
+    address public ribbitAddress;
+
+    constructor(address _froggyAddress) {
+        froggyAddress = _froggyAddress;
+    }
+
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes calldata
+    ) external pure override returns (bytes4) {
+        return IERC721Receiver.onERC721Received.selector;
+    }
+}
