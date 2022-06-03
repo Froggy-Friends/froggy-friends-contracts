91c557a7095d85bf54737da967898efeb95d4cd8
alfonsoaranzazu <alfonso@viruett.com>
2022-06-03 16:46:20 -0700

    adds code comments

--- contracts/RibbitPrime.sol
+++ contracts/RibbitPrime.sol
@@ -366,10 +366,13 @@ contract RibbitPrime is Context, ERC165, IERC1155, IERC1155MetadataURI, Ownable 
 		return string(abi.encodePacked(baseUrl, Strings.toString(_tokenId)));
 	}
 
+    /// @notice sets the metadata base url
+    /// @param _baseUrl the metadata base url for example 'https://api.froggyfriendsnft.com/item/'
 	function setURI(string memory _baseUrl) public onlyOwner {
 		baseUrl = _baseUrl;
 	}
 
+    /// @dev See {IERC1155-balanceOfBatch}.
 	function balanceOfBatch(address[] memory accounts, uint256[] memory ids) public view virtual override returns (uint256[] memory) {
 		require(accounts.length == ids.length, "ERC1155: accounts and ids length mismatch");
 
@@ -382,30 +385,22 @@ contract RibbitPrime is Context, ERC165, IERC1155, IERC1155MetadataURI, Ownable 
 		return batchBalances;
 	}
 
-    /**
-	 * @dev See {IERC165-supportsInterface}.
-	 */
+    /// @dev See {IERC165-supportsInterface}.
 	function supportsInterface(bytes4 interfaceId) public view virtual override(ERC165, IERC165) returns (bool) {
 		return interfaceId == type(IERC1155).interfaceId || interfaceId == type(IERC1155MetadataURI).interfaceId || super.supportsInterface(interfaceId);
 	}
 
-	/**
-	 * @dev See {IERC1155-setApprovalForAll}.
-	 */
+	/// @dev See {IERC1155-setApprovalForAll}.
 	function setApprovalForAll(address operator, bool approved) public virtual override {
 		_setApprovalForAll(_msgSender(), operator, approved);
 	}
 
-	/**
-	 * @dev See {IERC1155-isApprovedForAll}.
-	 */
+	/// @dev See {IERC1155-isApprovedForAll}.
 	function isApprovedForAll(address account, address operator) public view virtual override returns (bool) {
 		return _operatorApprovals[account][operator];
 	}
 
-	/**
-	 * @dev See {IERC1155-safeTransferFrom}.
-	 */
+	/// @dev See {IERC1155-safeTransferFrom}.
 	function safeTransferFrom(address from, address to, uint256 id, uint256 amount, bytes memory data) public virtual override {
 		require(from == _msgSender() || isApprovedForAll(from, _msgSender()), "ERC1155: caller is not owner nor approved");
 
@@ -427,9 +422,7 @@ contract RibbitPrime is Context, ERC165, IERC1155, IERC1155MetadataURI, Ownable 
 		}
 	}
 
-	/**
-	 * @dev See {IERC1155-safeBatchTransferFrom}.
-	 */
+	//// @dev See {IERC1155-safeBatchTransferFrom}.
 	function safeBatchTransferFrom(address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) public virtual override {
 		require(from == _msgSender() || isApprovedForAll(from, _msgSender()), "ERC1155: transfer caller is not owner nor approved");
 		_safeBatchTransferFrom(from, to, ids, amounts, data);
