193b6d3a020eeae914800c2ab94c118ceb109004
alfonsoaranzazu <alfonso@viruett.com>
2022-06-10 11:56:03 -0700

    adds deploy collab land contract

---
+++ scripts/deploy-collabland.ts
@@ -0,0 +1,41 @@
+import { ethers, run } from "hardhat";
+
+
+function sleep(ms: number) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms);
+    })
+}
+
+async function main() {
+    console.log("Starting deployment...");
+    const factory = await ethers.getContractFactory("FroggyCollabLand");
+    const [owner] = await ethers.getSigners();
+    console.log("\nDeployment Owner: ", owner.address);
+
+    const contract = (await factory.deploy());
+    console.log("\nContract Address: ", contract.address);
+    
+    await contract.deployed();
+    console.log("\nContract deployed...");
+
+    await contract.deployTransaction.wait(5);
+    console.log("\nContract deployed with 5 confirmations waiting 60 seconds to verify...");
+
+    await sleep(60000);
+
+    console.log("\nPublishing and verifying code to Etherscan...");
+    await run("verify:verify", 
+        { 
+            address: contract.address,
+            constructorArguments: []
+        }
+    );
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.log(error);
+        process.exit(1);
+    });
