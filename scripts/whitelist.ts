5f96c38d391fa93a6437c9cf09366b6d59f97e81
alfonsoaranzazu <alfonso@viruett.com>
2023-03-18 05:38:32 -0700

    updates metadata

--- scripts/whitelist.ts
+++ scripts/whitelist.ts
@@ -15,3 +15,5 @@ for (const wallet of holders) {
 let whitelist = new MerkleTree(hashedWallets, keccak256, { sortPairs: true });
 
 console.log("Holders root: ", whitelist.getHexRoot());
+let proof = whitelist.getHexProof(keccak256('0x6b01aD68aB6F53128B7A6Fe7E199B31179A4629a'));
+console.log("Holder proof: ", proof);
